021dc21b722b4f413c9872a16cc078f362eae8da
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-24 17:15:05 +0200

    concatenation & template-string

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-//console.log('Hello hello world');
-//console.error('this is error message');
-//console.warn('this is warning message');
+/* console.log('Hello hello world');
+console.error('this is error message');
+console.warn('this is warning message'); */
 
 
 // var, let, const
-//let age = 90;
-//age =  100;
-//console.log(age);
+/* let age = 90;
+age =  100;
+console.log(age);
 
-//const pi = 3.14;
-//console.log(pi);
+const pi = 3.14;
+console.log(pi); */
 
-const name ='Meh';
+/* const name ='Meh';
 const age = 25;
 const rating = 4.5;
 const isCool = true;
@@ -25,4 +25,17 @@ console.log(typeof rating);
 console.log(typeof isCool);
 console.log(typeof x);
 console.log(typeof y);
-console.log(typeof z);
+console.log(typeof z); */
+
+
+//CONCATENATION
+let name = "Meh";
+let age = 25
+console.log('My name is '+ name + " and my age is " + age);
+//TEMPLATE STRING
+console.log(`My name is ${name} and my age is ${age}`);
+//OR
+const intro = `My name is ${name} and my age is ${age}`;
+console.log(intro);
+
+
