c8291195d6fd7448d2085bcf35c8b9f02009f2a7
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-24 17:05:23 +0200

    typeof_variables

--- main.js
+++ main.js
@@ -4,9 +4,25 @@
 
 
 // var, let, const
-let age = 90;
-age =  100;
-console.log(age);
+//let age = 90;
+//age =  100;
+//console.log(age);
 
-const pi = 3.14;
-console.log(pi);
+//const pi = 3.14;
+//console.log(pi);
+
+const name ='Meh';
+const age = 25;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
