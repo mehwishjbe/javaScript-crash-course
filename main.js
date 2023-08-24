fbcf9ffc3fcb405e9e832d24e1d495765a4f1994
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-24 16:59:00 +0200

    let&const

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-console.log('Hello hello world');
-console.error('this is error message');
-console.warn('this is warning message');
+//console.log('Hello hello world');
+//console.error('this is error message');
+//console.warn('this is warning message');
+
+
+// var, let, const
+let age = 90;
+age =  100;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
