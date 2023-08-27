9fc7607832cbf5abbc38d1319fae010a5957af12
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-27 13:59:15 +0200

    prototype

--- main.js
+++ main.js
@@ -313,13 +313,17 @@ function Person(firstName, lastName, dob){
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function(){
-        return this.dob.getFullYear();
-    }
+    
+}
 
-    this.getFullName = function(){
+//PROTOTYPE
+Person.prototype.getBirthYear = function(){
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function(){
     return `${this.firstName} ${this.lastName}`;
-    }
+
 }
 
 //INSTANTIATE OBJECT
